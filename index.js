a7cb46b278b8c21f214432fd387990dc3212df94
gonzamcprojects <gonzalojmarquezc@gmail.com>
2026-05-07 15:52:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function obtenerhora() {
 }
 
 app.get("/", (req, res) => {
-  res.send(obtenerMensaje());
+  res.send(`$(obtenerMensaje()<br>Hora actual: $(obtenerHora()}`);
 });
 
 app.get("/health", (req, res) => res.status(200).send("ok"));
